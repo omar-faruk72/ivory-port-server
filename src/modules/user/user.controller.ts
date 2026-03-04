733b9ca254f3c8bff558d29317cd33a36ea6ab48
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-04 16:24:25 +0600

    added an user controllers login user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -19,6 +19,26 @@ const createUserController = async (req: Request, res: Response) => {
     }
 };
 
+// loin user 
+const loginUserController = async (req: Request, res: Response) => {
+    try {
+        const loginData = req.body;
+        const result = await userService.loginUser(loginData);
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful!",
+            data: result,
+        });
+    } catch (err: any) {
+        res.status(err.statusCode || 500).json({
+            success: false,
+            message: err.message || "Login failed!",
+        });
+    }
+};
+
 export const userController = {
     createUserController,
+    loginUserController,
 };
