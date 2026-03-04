a101c11b303af563b46bba33f870018039a60514
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-03-04 20:45:51 +0600

    get alll services service file code"

--- src/modules/service/service.service.ts
+++ src/modules/service/service.service.ts
@@ -14,6 +14,30 @@ const createService = async (serviceData: any) => {
     }
 };
 
+// get all service
+const getAllServices = async (page: number, limit: number) => {
+    try {
+        const skip = (page - 1) * limit;
+        const result = await collections.servicesCollection
+            .find()
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+        const total = await collections.servicesCollection.countDocuments();
+        return {
+            services: result,
+            meta: {
+                page,
+                limit,
+                total,
+                totalPage: Math.ceil(total / limit)
+            }
+        };
+    } catch (err) {
+        throw err;
+    }
+};
 export const serviceServices = {
     createService,
+    getAllServices,
 }
