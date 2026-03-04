1c57424f7572c113a894a43220c94494d00ed754
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-03-04 21:17:45 +0600

    update service api successfully working

--- src/modules/service/service.routes.ts
+++ src/modules/service/service.routes.ts
@@ -9,4 +9,6 @@ router.get("/all-services", serviceControllers.getAllServicesController);
 
 router.get("/services/:id", serviceControllers.getSingleServiceController);
 
+router.put("/services/:id", serviceControllers.updateServiceController);
+
 export const serviceRoutes = router;
