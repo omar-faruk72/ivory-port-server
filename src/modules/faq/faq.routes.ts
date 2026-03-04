5e1e1149185ea808c8adc802f840ba288a31d352
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-03-04 22:12:14 +0600

    faq delete api createt

--- src/modules/faq/faq.routes.ts
+++ src/modules/faq/faq.routes.ts
@@ -10,4 +10,6 @@ routes.get("/all-faqs", faqControllers.getAllFAQsController);
 routes.get("/faq/:id", faqControllers.getSingleFAQController);
 
 routes.put("/faq/:id", faqControllers.updateFAQController);
+
+routes.delete("/faq/:id", faqControllers.deleteFAQController);
 export const faqRoutes = routes;
