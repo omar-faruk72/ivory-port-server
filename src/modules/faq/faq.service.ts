6a70a5c408dd99a7eef8ac771c70fd347aa51fac
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-03-04 21:45:51 +0600

    get all faq service

--- src/modules/faq/faq.service.ts
+++ src/modules/faq/faq.service.ts
@@ -14,6 +14,32 @@ const createFAQ = async (faqData: { question: string; answer: string }) => {
     }
 };
 
+// get all faq
+const getAllFAQs = async (page: number, limit: number) => {
+    try {
+        const skip = (page - 1) * limit;
+        const faqs = await collections.faqsCollection
+            .find()
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+        const total = await collections.faqsCollection.countDocuments();
+
+        return {
+            faqs,
+            meta: {
+                page,
+                limit,
+                total,
+                totalPage: Math.ceil(total / limit)
+            }
+        };
+    } catch (err) {
+        throw err;
+    }
+};
+
 export const faqServices = {
     createFAQ,
+    getAllFAQs,
 }
