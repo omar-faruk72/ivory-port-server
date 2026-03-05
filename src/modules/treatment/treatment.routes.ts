93d7340d594cd00acbdef5170e2089bd20dc1c18
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-03-05 21:32:38 +0600

    get all treatment api successfully working

--- src/modules/treatment/treatment.routes.ts
+++ src/modules/treatment/treatment.routes.ts
@@ -4,4 +4,6 @@ import { treatmentControllers } from "./treatment.controller";
 const router = Router();
 
 router.post("/add-treatments", treatmentControllers.addTreatmentController);
+
+router.get("/all-treatments", treatmentControllers.getAllTreatmentController);
 export const treatmentRouters = router;
