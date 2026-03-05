f40193d73c93af550266d6adc3ba4f3510ee73fe
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-03-05 22:10:56 +0600

    get all treatment list service

--- src/modules/treatment-list/treatment-list.service.ts
+++ src/modules/treatment-list/treatment-list.service.ts
@@ -14,6 +14,32 @@ const addTreatmentList = async (treatmentData: any) => {
     }
 };
 
+// get all treatment list
+const getAllTreatmentsList = async (page: number, limit: number) => {
+    try {
+        const skip = (page - 1) * limit;
+        const treatments = await collections.treatmentListCollection
+            .find()
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+        const total = await collections.treatmentListCollection.countDocuments();
+
+        return {
+            treatments,
+            meta: {
+                page,
+                limit,
+                total,
+                totalPage: Math.ceil(total / limit)
+            }
+        };
+    } catch (err) {
+        throw err;
+    }
+};
+
 export const treatmentListServices = {
     addTreatmentList,
+    getAllTreatmentsList,
 }
