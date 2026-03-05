806b624a54694e9d97e96692c8b36fd631b552a5
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-03-05 20:39:02 +0600

    get all gallery service

--- src/modules/gallery/gallery.service.ts
+++ src/modules/gallery/gallery.service.ts
@@ -14,6 +14,32 @@ const addGallery = async (imageData: any) => {
     }
 };
 
+// get all gallery
+const getAllGallery = async (page: number, limit: number) => {
+    try {
+        const skip = (page - 1) * limit;
+        const images = await collections.galleryCollection
+            .find()
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+        const total = await collections.galleryCollection.countDocuments();
+
+        return {
+            images,
+            meta: {
+                page,
+                limit,
+                total,
+                totalPage: Math.ceil(total / limit)
+            }
+        };
+    } catch (err) {
+        throw err;
+    }
+};
+
 export const galleryServices = {
     addGallery,
+    getAllGallery,
 }
