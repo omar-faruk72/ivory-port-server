d4a2a4636cf894e3dae28458f3b1358a2023e347
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-03-05 21:47:13 +0600

    update treatment api successfully workign

--- src/modules/treatment/treatment.routes.ts
+++ src/modules/treatment/treatment.routes.ts
@@ -7,5 +7,7 @@ router.post("/add-treatments", treatmentControllers.addTreatmentController);
 
 router.get("/all-treatments", treatmentControllers.getAllTreatmentController);
 
-router.get("/treatment/:id", treatmentControllers.getSingleTreatmentController)
+router.get("/treatment/:id", treatmentControllers.getSingleTreatmentController);
+
+router.put("/treatment/:id", treatmentControllers.updateTreatmentController);
 export const treatmentRouters = router;
